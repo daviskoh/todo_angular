a7f8ba05bbe2e79a6d72fb018029c3e80a5803a6
daviskoh <koh.davis.0@gmail.com>
2014-03-16 00:13:04 -0400

    renderes todos

--- todos.js
+++ todos.js
@@ -2,5 +2,8 @@ angular.module('todos', [])
   .controller('TodoController', function() {
     this.totalTodos = 4;
 
-
+    this.todos = [
+      {text:'do stuff', done:false},
+      {text:'do more stuff', done:false}
+    ];
   });
