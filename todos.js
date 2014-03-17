2d326ed5d5344896b14f6353379245dc1853d05e
daviskoh <koh.davis.0@gmail.com>
2014-03-17 14:48:45 -0400

    revealing?

--- todos.js
+++ todos.js
@@ -1,24 +1,30 @@
-// assign to var incase more controllers necessary later
-var todoApp = angular.module('todos', []);
+var todos = (function() {
+  // assign to var incase more controllers necessary later
+  var todoApp = angular.module('todos', []);
 
-todoApp.controller('TodoController', function() {
-  this.todos = [
-    {text:'do stuff', done:false},
-    {text:'do more stuff', done:false}
-  ];
+  todoApp.controller('TodoController', function() {
+    this.todos = [
+      {text:'do stuff', done:false},
+      {text:'do more stuff', done:false}
+    ];
 
-  this.getTotalTodos = function() {
-    return this.totalTodos = this.todos.length;
-  };
+    this.getTotalTodos = function() {
+      return this.totalTodos = this.todos.length;
+    };
 
-  this.addTodo = function() {
-    this.todos.push({text:this.formTodoText, done:false});
-    this.formTodoText = '';
-  };
+    this.addTodo = function() {
+      this.todos.push({text:this.formTodoText, done:false});
+      this.formTodoText = '';
+    };
 
-  this.clearCompleted = function() {
-    this.todos = this.todos.filter(function(todo) {
-      return !todo.done;
-    });
-  };
-});
+    this.clearCompleted = function() {
+      this.todos = this.todos.filter(function(todo) {
+        return !todo.done;
+      });
+    };
+  });
+
+  return {
+    todoApp: todoApp
+  }
+})();
