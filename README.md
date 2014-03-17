481bc61f79bd4c2f955f48d0ea607d82f22d2f5e
daviskoh <koh.davis.0@gmail.com>
2014-03-17 15:30:45 -0400

    test

---
+++ README.md
@@ -0,0 +1,1 @@
+## Basic client-side todo application built with AngularJS
--- todos.js
+++ todos.js
@@ -1,30 +1,39 @@
-var todos = (function() {
-  // assign to var incase more controllers necessary later
-  var todoApp = angular.module('todos', []);
+// declare var incase there is need for more controllers later
+var todoApp = angular.module('todos', []);
 
-  todoApp.controller('TodoController', function() {
-    this.todos = [
-      {text:'do stuff', done:false},
-      {text:'do more stuff', done:false}
-    ];
+todoApp.controller('TodoController', function() {
+  var self = this;
 
-    this.getTotalTodos = function() {
-      return this.totalTodos = this.todos.length;
-    };
+  this.todos = [
+    {text:'do stuff', done:false},
+    {text:'do more stuff', done:false},
+    {text:'test', done:false}
+  ];
 
-    this.addTodo = function() {
+  this.getTotalTodos = function() {
+    return this.totalTodos = this.todos.length;
+  };
+
+  this.todoExist = function(todoText) {
+    return self.todos.some(function(todo) {
+      return todo.text === todoText;
+    });
+  };
+
+  this.addTodo = function() {
+    if (!this.todoExist(this.formTodoText)) {
+      console.log('adding Todo');
       this.todos.push({text:this.formTodoText, done:false});
-      this.formTodoText = '';
     };
+    
+    this.formTodoText = '';
+  };
 
-    this.clearCompleted = function() {
-      this.todos = this.todos.filter(function(todo) {
-        return !todo.done;
-      });
-    };
-  });
+  this.clearCompleted = function() {
+    console.log('clearing Completed');
 
-  return {
-    todoApp: todoApp
-  }
-})();
+    this.todos = this.todos.filter(function(todo) {
+      return !todo.done;
+    });
+  };
+});
