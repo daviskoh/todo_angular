f1e10235c4f6c9845c49ade3991e10d3b319c182
daviskoh <koh.davis.0@gmail.com>
2014-03-17 15:38:10 -0400

    adequate

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,22 +1,34 @@
-// assign to var incase more controllers necessary later
+// declare var incase there is need for more controllers later
 var todoApp = angular.module('todos', []);
 
 todoApp.controller('TodoController', function() {
   this.todos = [
-    {text:'do stuff', done:false},
-    {text:'do more stuff', done:false}
+    {text:'task 1', done:false},
+    {text:'task 2', done:false}
   ];
 
   this.getTotalTodos = function() {
     return this.totalTodos = this.todos.length;
   };
 
+  this.todoExist = function(todoText) {
+    return this.todos.some(function(todo) {
+      return todo.text === todoText;
+    });
+  };
+
   this.addTodo = function() {
-    this.todos.push({text:this.formTodoText, done:false});
+    if (!this.todoExist(this.formTodoText)) {
+      console.log('adding: %s', this.formTodoText);
+      this.todos.push({text:this.formTodoText, done:false});
+    };
+    
     this.formTodoText = '';
   };
 
   this.clearCompleted = function() {
+    console.log('clearing Completed');
+
     this.todos = this.todos.filter(function(todo) {
       return !todo.done;
     });
